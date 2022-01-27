de6852a223a1c909ceea1b1f1f8af96d07ae0965
ion.ciubaciuc <20557635+ion-ciubaciuc@users.noreply.github.com>
2022-01-27 13:40:38 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main class="flex flex-col items-center">
+  <h1 class="text-5xl uppercase font-light py-8 px-4 text-orange-600">
+    Hello {name}!
+  </h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial" class="underline">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
